0984335c6e11dd9be8a859213b9001ce64a4f2f9
blazor67 <blazor67@users.noreply.github.com>
2021-05-23 17:07:46 +0200

    mi ero scordato i file
    lol

---
+++ script.js
@@ -0,0 +1,99 @@
+let ape = document.querySelector("#ape");
+let fungo = document.querySelector("#fungo");
+let punt = document.querySelector("#punt"); //audio
+let bott = document.querySelector("#assicuta"); //bott
+
+function randomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+  
+
+//variabili randompos
+let apeLeft;
+let apeTop;
+let fungoLeft;
+let fungoTop;
+
+//ape randompos
+apeLeft=randomInt(window.innerWidth)-10; 
+apeTop=randomInt(window.innerHeight)-10;
+ape.style.left=apeLeft+"px";
+ape.style.top=apeTop+"px";
+
+//fungo randompos
+fungoLeft=randomInt(window.innerWidth)-10; 
+fungoTop=randomInt(window.innerHeight)-10;
+fungo.style.left=fungoLeft+"px";
+fungo.style.top=fungoTop+"px";
+
+//movimento
+
+function assicuta(){
+
+    //console.log("assicuta");
+    //distanziamento sociale
+    hDist=fungoLeft-apeLeft;
+    vDist=fungoTop-apeTop;
+
+    //calcola
+    apeLeft=apeLeft+(hDist*.1)+Math.sign(hDist)*4;
+    apeTop=apeTop+(vDist*.1)+Math.sign(vDist)*4;
+
+    //assegna
+    ape.style.left=apeLeft+"px";
+    ape.style.top=apeTop+"px";
+
+    
+    //console.log("mi avvicino, la distanza è di "+hDist+" e "+vDist);
+    return 0;
+}
+
+//check overlap di due item
+function overlap(){
+    if(Math.abs(fungoLeft-apeLeft)<=4 && Math.abs(fungoTop-apeTop)<=4){
+        return true;
+    } else{
+        assicuta();
+        return false;
+    }
+}
+
+//dopo la punturina
+function crescita(){
+    punt.play();
+
+    //il fungo cresce
+    setTimeout(function(){
+        punt.src="lungo.wav";
+        punt.play();
+        fungo.style.transition="2s ease-in";
+        fungo.classList.add("lungo");
+    },500);
+
+    //ok ora puoi ricaricare
+    setTimeout(function(){        
+        bott.innerHTML="Ora hai il fungo lungo.<br>Clicca qui per ricaricare.";
+        bott.onclick=window.location.reload.bind(window.location);
+        bott.style.display="initial";
+    },3000);
+}
+
+//ciclo di check se ci sei sul fungo
+function ciclo(){
+    setTimeout(function(){
+        if(!overlap()){
+            //console.log("suca");
+            ciclo();
+        } else {
+            crescita();
+        }
+    },40);
+}
+
+function bottoneFungo(){
+
+    bott.style.display="none";
+    ciclo();
+}
+
+bott.onclick=bottoneFungo;
